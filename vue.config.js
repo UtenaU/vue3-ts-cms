32355284441241d8ffa63c77ec50d55312cccbe4
Nadia <wzmjohn2018@163.com>
2022-09-21 21:23:03 +0800

    build(vueconfigjs): webpack config methods

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,30 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  //配置方式一：CLI提供的属性
+  outputDir: './dist',
+  //配置方式二： 和webpacl属性完全一致，最后会进行合并
+  // configureWebpack:{
+  //   resolve:{
+  //     alias: {
+  //       components: '@/components'
+  //     }
+  //   }
+  // }
+  // configureWebpack: (config) => {
+  //   config.resolve.alias = {
+  //     '@': path.resolve(__dirname, 'src'),
+  //     components: '@/components'
+  //   }
+  // }
+  //配置方式三：链式
+  chainWebpack: (config) => {
+    config.resolve.alias
+    .set('@', path.resolve(__dirname, 'src'))
+    .set('components', '@/components')
+  }
+
 })
+
+
