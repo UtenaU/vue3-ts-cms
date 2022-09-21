260ded98c3466f80ff63b54564cd6773be1149e8
Nadia <wzmjohn2018@163.com>
2022-09-21 21:46:21 +0800

    feat(app): use vue-router

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import router from './router'
 
-console.log('hhhhhhhhh')
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
